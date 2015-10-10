c792b9c7928202205c5cd5fa9ec72683f291083e
Michael <mike_visualsoft@yahoo.com>
2015-10-10 10:45:19 +0700

    Add

--- api/controllers/StartupController.js
+++ api/controllers/StartupController.js
@@ -15,9 +15,11 @@ module.exports = {
 		Startup.findOne({'name':req.param('name')}, function(err,startup){
 				if(err) return next(err);
 				if(!startup) return res.redirect('/');
+				var tmp_name = startup.name;
+				tmp_name = tmp_name.charAt(0).toUpperCase() + tmp_name.slice(1);
 				return res.view({
 						startup : startup,
-						title : startup.name + ' | Startup Mikroskil',
+						title : tmp_name + ' | Startup Mikroskil',
 						image : 'http://startupmikroskil.herokuapp.com/images/kolexia/logo.jpg',
 						description : startup.description,
 						url : 'http://www.startupmikroskil.herokuapp.com/startup/'+startup.name
